b279bff43f5a05d9e4c56cd98a7232adbe9b6619
Sam Blackman <sblackman@gmail.com>
2018-11-01 00:07:31 -0400

    Fix tests for new tax table structure

--- src/calculateTax.test.js
+++ src/calculateTax.test.js
@@ -2,25 +2,26 @@ import calculateTax from './calculateTax'
 import taxTables from './data/taxTables'
 
 describe('calculateTax', () => {
+  const mjfTable = taxTables["marriedFilingJointly"]
   describe('for married filing jointly', () => {
     it('calculates the tax for income less than the lowest bracket', () => {
-      expect(calculateTax(10000, taxTables)).toEqual(1000)
+      expect(calculateTax(10000, mjfTable)).toEqual(1000)
     })
 
     it('calculates the tax for income exactly at the lowest bracket', () => {
-      expect(calculateTax(19050, taxTables)).toEqual(1905)
+      expect(calculateTax(19050, mjfTable)).toEqual(1905)
     })
 
     it('calculates the tax for income exactly between brackets', () => {
-      expect(calculateTax(20000, taxTables)).toEqual(2019)
+      expect(calculateTax(20000, mjfTable)).toEqual(2019)
     })
 
     it('calculates the tax for income exactly at a middle bracket', () => {
-      expect(calculateTax(77400, taxTables)).toEqual(8907)
+      expect(calculateTax(77400, mjfTable)).toEqual(8907)
     })
 
     it('calculates the tax for income in the highest bracket', () => {
-      expect(calculateTax(800000, taxTables)).toEqual(235379)
+      expect(calculateTax(800000, mjfTable)).toEqual(235379)
     })
   })
 })
