a95e9ccf82602505d7118c2793ff3f3f45ca2346
Sam Blackman <sblackman@gmail.com>
2019-04-10 12:55:41 -0400

    Fix warnings

--- src/DisplayForm.js
+++ src/DisplayForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Field from './Field';
-import taxTables from './data/taxTables'
+import taxTables from './data/taxTables2018'
 import calculateTax from './calculateTax'
 
 class EstimatedTaxForm extends Component {
@@ -19,17 +19,19 @@ class EstimatedTaxForm extends Component {
         <h2>2018 Estimated Tax Worksheet</h2>
         <form onSubmit={this.handleSubmit}>
           <table>
-            {this.fields.map((field, i) =>
-              <tr>
-                <td><span className='DisplayForm-number'>{field.name}. </span>{field.instruction}</td>
-                <td>
-                <input
-                  disabled={true}
-                  value={this.displayFromState(field)}
-                  className='DisplayForm-field' />
-                </td>
-              </tr>
-            )}
+            <tbody>
+              {this.fields.map((field, i) =>
+                <tr key={field.name}>
+                  <td><span className='DisplayForm-number'>{field.name}. </span>{field.instruction}</td>
+                  <td>
+                  <input
+                    disabled={true}
+                    value={this.displayFromState(field)}
+                    className='DisplayForm-field' />
+                  </td>
+                </tr>
+              )}
+            </tbody>
           </table>
         </form>
       </div>
